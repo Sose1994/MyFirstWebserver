a9f3cf81d23fd4f31407ee2b8b5916083a3989be
Sose Sahakian <sosesahakian@Soses-MacBook-Pro.local>
2017-08-25 12:47:48 +0200

    initial commit

---
+++ server.js
@@ -0,0 +1,10 @@
+//http
+var http = require('http');
+
+var server = http.createServer(function(req, res){
+    res.writeHead(200, {'Content-type' : 'text/html'});
+    res.write('{title" : "Hello JSON web API", "text" : "Text fra min API"');
+    res.end();
+});
+
+server.listen(3003);
