035fbd29a6f5e2567b9b3c6a9999e17e82653a85
Sose <sose94@gmail.com>
2017-09-08 11:51:39 +0200

    stuff added

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 //http
 var http = require('http');
+var fs = require('fs');
 
-var server = http.createServer(function(req, res){
-    res.writeHead(200, {'Content-type' : 'text/html'});
-    res.write('{title" : "Hello JSON web API", "text" : "Text fra min API"');
-    res.end();
-});
+fs.readFile('medium.txt', 'utf-8', function(err, data) {
+    var server = http.createServer(function(req, res){
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.writeHead(200, {'Content-type' : 'text/html'});
+        res.write(data);
+        res.end();
+    });
+})
 
 //server.listen(3003);
 server.listen(process.env.PORT || 3003);
