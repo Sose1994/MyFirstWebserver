34125eefc4fb3c396b0cd240a6d191c11bf56b34
Sose <sose94@gmail.com>
2017-09-22 09:42:48 +0200

    added json to express stuff

--- server.js
+++ server.js
@@ -1,16 +1,23 @@
 //http
 var http = require('http');
 var fs = require('fs');
+var colors = require('colors');
+var oneLinerJoke = require('one-liner-joke');
+
+console.log('Starting server...'.zebra);
+var getRandomJoke = oneLinerJoke.getRandomJoke();
 
 var server = http.createServer(function(req, res){
 
-	fs.readFile('medium.txt', 'utf-8', function(err, data) {
+	fs.readFile('text.txt', 'utf-8', function(err, data) {
         res.setHeader('Access-Control-Allow-Origin', '*');
         res.writeHead(200, {'Content-type' : 'text/html'});
         res.write(data);
+        res.write('<br /> <br />' + getRandomJoke.body);
         res.end();
     });
 })
+
 
 //server.listen(3003);
 server.listen(process.env.PORT || 3003);
